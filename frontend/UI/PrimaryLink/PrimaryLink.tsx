41ca8ed0586db120614a319765efa7b5901b217e
NextKolya <phelixgluy@gmail.com>
2025-11-01 13:19:47 +0600

    Add currentChat logic for PrimaryLink isActive in ChatList

--- frontend/UI/PrimaryLink/PrimaryLink.tsx
+++ frontend/UI/PrimaryLink/PrimaryLink.tsx
@@ -2,20 +2,30 @@ import type { AnchorHTMLAttributes } from 'react';
 
 import Link from 'next/link';
 
-import buttonStyles from './PrimaryLink.module.scss';
+import clsx from 'clsx';
+import linkStyles from './PrimaryLink.module.scss';
 
 interface PrimaryLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
     title: string;
     href: string;
+
+    isActive: boolean;
 }
 export default function PrimaryLink({
     title,
+    isActive,
 
     ...attributes
 }: PrimaryLinkProps) {
     return (
-        <Link {...attributes} className={buttonStyles['primary-button']}>
-            <span className={buttonStyles['title']}>{title}</span>
+        <Link
+            {...attributes}
+            className={clsx(
+                linkStyles['primary-link'],
+                isActive && linkStyles['active']
+            )}
+        >
+            <span className={linkStyles['title']}>{title}</span>
         </Link>
     );
 }
--- frontend/components/Navbar/components/ChatList/ChatList.tsx
+++ frontend/components/Navbar/components/ChatList/ChatList.tsx
@@ -1,13 +1,22 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import PrimaryLink from '@/UI/PrimaryLink';
+
 import navStyles from './ChatList.module.scss';
 
 const chatList = [
     {
-        id: crypto.randomUUID(),
-        name: 'Initial chat',
+        id: '96697d9d-2749-4ced-9162-0701abf4337a',
+        name: 'Initial chat initial initial initial initial',
     },
 ];
 
 export default function ChatList() {
+    const pathname = usePathname();
+    const currentChatId = pathname.split('/').filter(Boolean).at(-1);
+
     return (
         <div className={navStyles['chats-box']}>
             <div className={navStyles['title-block']}>
@@ -20,7 +29,13 @@ export default function ChatList() {
 
             <ul className={navStyles['chat-list']}>
                 {chatList.map((chat) => (
-                    <li key={chat.id} className={navStyles['chat-link']}></li>
+                    <li key={chat.id} className={navStyles['chat-link']}>
+                        <PrimaryLink
+                            title={chat.name}
+                            href={`/chat/${chat.id}`}
+                            isActive={currentChatId === chat.id}
+                        />
+                    </li>
                 ))}
             </ul>
         </div>
