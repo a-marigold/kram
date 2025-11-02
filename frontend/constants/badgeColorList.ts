6673652251b31e4262d501ef6892752277263bd6
NextKolya <phelixgluy@gmail.com>
2025-11-02 13:46:15 +0600

    Move badgeColorList to constants in frontend root

---
+++ frontend/constants/badgeColorList.ts
@@ -0,0 +1,18 @@
+export const badgeColorList = [
+    {
+        bgColor: 'var(--badge-purple-bg)',
+        fontColor: 'var(--badge-purple-font-color)',
+    },
+    {
+        bgColor: 'var(--badge-green-bg)',
+        fontColor: 'var(--badge-green-font-color)',
+    },
+    {
+        bgColor: 'var(--badge-orange-bg)',
+        fontColor: 'var(--badge-orange-font-color)',
+    },
+    {
+        bgColor: 'var(--badge-blue-bg)',
+        fontColor: 'var(--badge-blue-font-color)',
+    },
+];
