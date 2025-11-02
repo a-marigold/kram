8857b82622432e454f62c18255904d2b4af66766
NextKolya <phelixgluy@gmail.com>
2025-11-02 15:37:57 +0600

    Delete toolButtonList. Add message tool buttons markup and styles

--- frontend/app/chat/[chatId]/components/MessageList/components/MessageMine.tsx
+++ frontend/app/chat/[chatId]/components/MessageList/components/MessageMine.tsx
@@ -2,6 +2,28 @@ import type { MessagePropsType } from './MessagePropsType';
 
 import messageStyles from './Message.module.scss';
 
+export const toolButtonlist = [
+    {
+        icon: (
+            <svg width={20} height={20}>
+                <use href='#clipboard-icon' />
+            </svg>
+        ),
+
+        ariaLabel: 'Copy message',
+    },
+
+    {
+        icon: (
+            <svg width={20} height={20}>
+                <use href='#edit-icon' />
+            </svg>
+        ),
+
+        ariaLabel: 'Edit message',
+    },
+];
+
 export function MessageMine({ children }: MessagePropsType) {
     return (
         <div className={messageStyles['message-block']}>
@@ -11,7 +33,17 @@ export function MessageMine({ children }: MessagePropsType) {
                 {children}
             </div>
 
-            <div className={messageStyles['tools-block']}></div>
+            <div className={messageStyles['tool-buttons-block']}>
+                {toolButtonlist.map((button, index) => (
+                    <button
+                        key={index}
+                        className={messageStyles['tool-button']}
+                        aria-label={button.ariaLabel}
+                    >
+                        {button.icon}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 }
--- frontend/app/chat/[chatId]/components/MessageList/components/MessageOther.tsx
+++ frontend/app/chat/[chatId]/components/MessageList/components/MessageOther.tsx
@@ -2,6 +2,38 @@ import type { MessagePropsType } from './MessagePropsType';
 
 import messageStyles from './Message.module.scss';
 
+export const toolButtonList = [
+    {
+        icon: (
+            <svg width={20} height={20}>
+                <use href='#clipboard-icon' />
+            </svg>
+        ),
+
+        ariaLabel: 'Copy message',
+    },
+
+    {
+        icon: (
+            <svg width={20} height={20}>
+                <use href='#like-icon' />
+            </svg>
+        ),
+
+        ariaLabel: 'EditMessage',
+    },
+
+    {
+        icon: (
+            <svg width={20} height={20}>
+                <use href='#dislike-icon' />
+            </svg>
+        ),
+
+        ariaLabel: 'EditMessage',
+    },
+];
+
 export function MessageOther({ children }: MessagePropsType) {
     return (
         <div className={messageStyles['message-block']}>
