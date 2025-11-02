7747e67c0704b7fc297510d67e1d738f2ddabddf
NextKolya <phelixgluy@gmail.com>
2025-11-02 12:27:30 +0600

    Add findBySymbol util

---
+++ frontend/utils/FindBySymbol/findBySymbol.ts
@@ -0,0 +1,22 @@
+/**
+ * @param {string} string - text for search
+ * @param {string} symbol - symbol, words with which they will be found
+ * @returns First or last word with symbol found in string. Finds word that starts with
+ *
+ * @example
+ * ```typescript
+ * findBySymbol('$aword Lorem ipsum dolor $lastword', '$') // Output: `@aword`
+ * findBySymbol('Lorem ipsum dolor #lastword', '#') // Output: `#lastword`
+ * ```
+ */
+
+export function findBySymbol(string: string, symbol: string) {
+    const firstWord = string.split(' ').filter(Boolean)[0];
+
+    const lastWord = string.split(' ').filter(Boolean).at(-1);
+    if (firstWord?.startsWith(string) && firstWord.length > 1) {
+        return firstWord;
+    } else if (lastWord?.startsWith(string) && lastWord.length > 1) {
+        return lastWord;
+    }
+}
