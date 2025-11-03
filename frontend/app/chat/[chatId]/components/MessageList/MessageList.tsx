fc84b07185f65bed15194014b0b07ed232a87357
NextKolya <phelixgluy@gmail.com>
2025-11-03 18:13:07 +0600

    More eslint corrections

--- frontend/app/chat/[chatId]/components/MessageList/MessageList.tsx
+++ frontend/app/chat/[chatId]/components/MessageList/MessageList.tsx
@@ -2,7 +2,7 @@ import { MessageMine, MessageOther } from './components';
 
 import messageStyles from './MessageList.module.scss';
 
-const __MESSAGE1_LIST__ = [
+const _MESSAGE1_LIST__ = [
     {
         type: 'mine',
 
@@ -126,24 +126,24 @@ const __MESSAGE1_LIST__ = [
     },
 ];
 
-const __MESSAGE2_LIST__ = [
-    {
-        type: 'mine',
+// const _MESSAGE2_LIST__ = [
+//     {
+//         type: 'mine',
 
-        text: 'Hello',
-    },
+//         text: 'Hello',
+//     },
 
-    {
-        type: 'other',
+//     {
+//         type: 'other',
 
-        text: '2',
-    },
-];
+//         text: '2',
+//     },
+// ];
 
 export default function MessageList() {
     return (
         <div className={messageStyles['message-list']}>
-            {__MESSAGE2_LIST__.map((message, __INDEX__) =>
+            {_MESSAGE1_LIST__.map((message, __INDEX__) =>
                 message.type === 'mine' ? (
                     <MessageMine key={__INDEX__}>{message.text}</MessageMine>
                 ) : (
