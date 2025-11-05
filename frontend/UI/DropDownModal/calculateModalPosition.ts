6370416850a23fc0554e53f6f7fee27adfebcf2d
NextKolya <phelixgluy@gmail.com>
2025-11-05 21:06:46 +0600

    Add calculateModalPosition in DropDownModal

---
+++ frontend/UI/DropDownModal/calculateModalPosition.ts
@@ -0,0 +1,42 @@
+export type ModalPosition = 'top' | 'right' | 'bottom' | 'left';
+
+export function calculateModalPosition(
+    relativeElement: HTMLElement,
+    modalElement: HTMLElement,
+    position: ModalPosition,
+    shiftX?: number,
+    shiftY?: number
+) {
+    const {
+        top: elemTop,
+        right: elemRight,
+        bottom: elemBottom,
+        left: elemLeft,
+        width: elemWidth,
+        height: elemHeight,
+    } = relativeElement.getBoundingClientRect();
+
+    const { width: modalWidth, height: modalHeight } =
+        modalElement.getBoundingClientRect();
+
+    let modalTop = 0;
+    let modalLeft = 0;
+
+    if (position === 'top') {
+        modalTop = elemTop - modalHeight;
+        modalLeft = elemLeft + elemWidth / 2 - modalWidth / 2;
+    } else if (position === 'right') {
+        modalTop = elemTop + elemHeight / 2 + modalHeight / 2;
+        modalLeft = elemLeft + elemWidth;
+    } else if (position === 'bottom') {
+        modalTop = elemTop + elemHeight;
+        modalLeft = elemLeft + elemWidth / 2 - modalWidth / 2;
+    } else if (position === 'left') {
+        modalTop = elemTop + elemHeight / 2 + modalHeight / 2;
+        modalLeft = elemLeft - modalWidth;
+    }
+
+    modalElement.style.transform = `translate(${modalLeft + (shiftX ?? 0)}px, ${
+        modalTop + (shiftY ?? 0)
+    }px)`;
+}
