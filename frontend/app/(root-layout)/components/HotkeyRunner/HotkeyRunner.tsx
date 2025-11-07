e60c604c7c88993374a59f8b843aa62de890ea82
NextKolya <phelixgluy@gmail.com>
2025-11-07 22:47:01 +0600

    Try to fix hotkeys + modal cast

--- frontend/app/(root-layout)/components/HotkeyRunner/HotkeyRunner.tsx
+++ frontend/app/(root-layout)/components/HotkeyRunner/HotkeyRunner.tsx
@@ -1,33 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import { useHotkeys } from '@/hooks/useHotkeys';
-
-import { useHotkeyStore } from '@/store/HotkeyStore';
 
 import { useHotkeyConfig } from './useHotkeyConfig';
 
 export default function HotkeyRunner() {
-    const registerHotkey = useHotkeyStore((state) => state.registerHotkey);
-    const unregisterHotkey = useHotkeyStore((state) => state.unregisterHotkey);
-
     useHotkeys();
 
-    const config = useHotkeyConfig();
-
-    useEffect(() => {
-        config.forEach(({ name, key, callback }) => {
-            registerHotkey(name, key, callback);
-            console.log(name);
-        });
-
-        return () => {
-            config.forEach(({ name }) => {
-                unregisterHotkey(name);
-            });
-        };
-    }, []);
+    useHotkeyConfig();
 
     return null;
 }
--- frontend/app/(root-layout)/components/HotkeyRunner/useHotkeyConfig.tsx
+++ frontend/app/(root-layout)/components/HotkeyRunner/useHotkeyConfig.tsx
@@ -1,36 +1,53 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import { useModalStore } from '@/store/ModalStore';
 
-import SearchModal from '../ModalRoot/modals/SearchModal';
-import SettingsModal from '../ModalRoot/modals/SettingsModal';
+import { useHotkeyStore } from '@/store/HotkeyStore';
 
 import type { Hotkey } from '@/types/Hotkey';
+
+import SearchModal from '@modals/SearchModal';
+import SettingsModal from '@modals/SettingsModal';
 
 export function useHotkeyConfig() {
     const openModal = useModalStore((state) => state.openModal);
     const closeModal = useModalStore((state) => state.closeModal);
 
-    const config: Hotkey[] = [
-        {
-            name: 'search',
-            key: 'O',
-            callback: () => openModal(<SearchModal closeModal={closeModal} />),
-        },
-        {
-            name: 'settings',
-            key: 'l',
-            callback: () => {
-                console.log(<SettingsModal closeModal={closeModal} />);
-                openModal(<SettingsModal closeModal={closeModal} />);
-            },
-        },
-        {
-            name: 'closeModal',
-            key: 'Esc',
-            callback: closeModal,
-        },
-    ];
+    const registerHotkey = useHotkeyStore((state) => state.registerHotkey);
+    const unregisterHotkey = useHotkeyStore((state) => state.unregisterHotkey);
 
-    return config;
+    useEffect(() => {
+        const config: Hotkey[] = [
+            {
+                name: 'search',
+                key: 'O',
+                callback: () =>
+                    openModal(<SearchModal closeModal={closeModal} />),
+            },
+            {
+                name: 'settings',
+                key: 'L',
+                callback: () => {
+                    openModal(<SettingsModal closeModal={closeModal} />);
+                },
+            },
+            {
+                name: 'closeModal',
+                key: 'Esc',
+                callback: closeModal,
+            },
+        ];
+
+        config.forEach(({ name, key, callback }) => {
+            registerHotkey(name, key, callback);
+        });
+
+        return () => {
+            config.forEach(({ name }) => {
+                unregisterHotkey(name);
+            });
+        };
+    }, []);
 }
