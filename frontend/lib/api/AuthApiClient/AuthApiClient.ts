1667045532c6b37043ef47a22265a007607f59b6
NextKolya <phelixgluy@gmail.com>
2025-11-08 13:56:23 +0600

    Add registerWithEmail in AuthApiClient

--- frontend/lib/api/AuthApiClient/AuthApiClient.ts
+++ frontend/lib/api/AuthApiClient/AuthApiClient.ts
@@ -1,1 +1,21 @@
-export function registerUser() {}
+import { apiOrigin } from '@/utils/GetApiOrigin';
+
+export async function registerWithEmail(email: string, password: string) {
+    const prepareUser = JSON.stringify({ email, password });
+
+    const response = await fetch(`${apiOrigin}/auth/register`, {
+        method: 'POST',
+        credentials: 'include',
+        body: prepareUser,
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+
+        throw new Error(errorData);
+    }
+
+    const data = await response.json();
+
+    return data;
+}
