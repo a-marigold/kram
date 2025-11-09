24db68dc1d57d1ff99bd6c0526d3c93e9a40e253
NextKolya <phelixgluy@gmail.com>
2025-11-09 13:18:37 +0600

    Add checkEmail and update registerWithEmail in AuthApiClient

--- frontend/lib/api/AuthApiClient/AuthApiClient.ts
+++ frontend/lib/api/AuthApiClient/AuthApiClient.ts
@@ -2,22 +2,38 @@ import type { ApiResponse } from 'shared';
 
 import { apiOrigin } from '@/utils/GetApiOrigin';
 
-export async function registerWithEmail(email: string, password: string) {
-    const prepareUser = JSON.stringify({ email, password });
+import { handleApiError } from '@/utils/HandleApiError';
 
-    const response = await fetch(`${apiOrigin}/auth/register`, {
+export async function checkEmail(email: string) {
+    const prepareEmail = JSON.stringify({ email });
+
+    const response = await fetch(`${apiOrigin}/auth/check-email`, {
         method: 'POST',
-        credentials: 'include',
-        body: prepareUser,
+
+        body: prepareEmail,
     });
 
-    if (!response.ok) {
-        const errorData: ApiResponse = await response.json();
-
-        throw new Error(errorData.message);
-    }
+    handleApiError(response);
 
     const data = await response.json();
 
     return data;
 }
+
+export async function registerWithEmail(email: string, password: string) {
+    const prepareUser = JSON.stringify({ email, password });
+
+    const response = await fetch(`${apiOrigin}/auth/register-email`, {
+        method: 'POST',
+        credentials: 'include',
+        body: prepareUser,
+    });
+
+    handleApiError(response);
+
+    const data = await response.json();
+
+    return data;
+}
+
+export async function loginWithEmail(email: string, password: string) {}
