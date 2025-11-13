74170a9f5defbbb970c50e3c1c3914c4b0a00447
NextKolya <phelixgluy@gmail.com>
2025-11-13 23:08:01 +0600

    Update userName schema in shared

--- shared/.dist/types/AuthorizationData.js
+++ shared/.dist/types/AuthorizationData.js
@@ -1,21 +1,20 @@
 import { object, string } from 'zod';
+const userNameSchema = string()
+    .nonempty('User name is required.')
+    .regex(/^[A-Za-zА-Яа-я][A-Za-zА-Я-а-я0-9_@#]*$/, 'User name must not begin with number, "@", "#" and contain spaces.')
+    .lowercase('User name must not contain captial letters');
+const passwordSchema = string()
+    .min(8, 'Password should be at least 8 characters')
+    .max(40, 'Password is too long');
 export const CheckUserDataSchema = object({
-    userName: string()
-        .min(1, 'User name is required')
-        .lowercase('User name must not contain captial letters'),
+    userName: userNameSchema,
 });
 export const RegisterDataSchema = object({
-    userName: string()
-        .min(1, 'User name is required')
-        .lowercase('User name must not contain capital letters'),
-    password: string()
-        .min(8, 'Password should be at least 8 characters')
-        .max(40, 'Password is too long'),
+    userName: userNameSchema,
+    password: passwordSchema,
     fullName: string().min(1, 'Name is required'),
 });
 export const LoginDataSchema = object({
-    userName: string()
-        .min(1, 'User name  is required')
-        .lowercase('User name must not contain capital letters'),
-    password: string().min(1, 'Password is required'),
+    userName: userNameSchema,
+    password: passwordSchema,
 });
--- shared/src/types/AuthorizationData.ts
+++ shared/src/types/AuthorizationData.ts
@@ -1,28 +1,31 @@
 import { object, email, string, infer as zinfer } from 'zod';
 
+const userNameSchema = string()
+    .nonempty('User name is required.')
+    .regex(
+        /^[A-Za-zА-Яа-я][A-Za-zА-Я-а-я0-9_@#]*$/,
+        'User name must not begin with number, "@", "#" and contain spaces.'
+    )
+    .lowercase('User name must not contain captial letters');
+
+const passwordSchema = string()
+    .min(8, 'Password should be at least 8 characters')
+    .max(40, 'Password is too long');
+
 export const CheckUserDataSchema = object({
-    userName: string()
-        .min(1, 'User name is required')
-        .lowercase('User name must not contain captial letters'),
+    userName: userNameSchema,
 });
 
 export const RegisterDataSchema = object({
-    userName: string()
-        .min(1, 'User name is required')
-        .lowercase('User name must not contain capital letters'),
+    userName: userNameSchema,
 
-    password: string()
-        .min(8, 'Password should be at least 8 characters')
-        .max(40, 'Password is too long'),
+    password: passwordSchema,
     fullName: string().min(1, 'Name is required'),
 });
 
 export const LoginDataSchema = object({
-    userName: string()
-        .min(1, 'User name  is required')
-        .lowercase('User name must not contain capital letters'),
-
-    password: string().min(1, 'Password is required'),
+    userName: userNameSchema,
+    password: passwordSchema,
 });
 
 export type CheckUserData = zinfer<typeof CheckUserDataSchema>;
