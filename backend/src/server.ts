ce7f35510197742e65e81d643a18c0a54e8421af
NextKolya <phelixgluy@gmail.com>
2025-11-16 16:41:30 +0600

    Add host in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ function start() {
     const app = buildApp();
 
     try {
-        app.listen({ port: PORT });
+        app.listen({ port: PORT, host: '0.0.0.0' });
     } catch (error) {
         app.log.error(error);
         process.exit(1);
