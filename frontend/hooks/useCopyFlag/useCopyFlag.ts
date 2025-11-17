85d14a0ff858ad1463107aac42fdddfd1c0c0fd6
NextKolya <phelixgluy@gmail.com>
2025-11-17 19:31:26 +0600

    Add documentation for useCopyFlag

--- frontend/hooks/useCopyFlag/useCopyFlag.ts
+++ frontend/hooks/useCopyFlag/useCopyFlag.ts
@@ -1,5 +1,33 @@
 import { useState, useEffect, useRef } from 'react';
 
+/**
+ * Hook to manage a temporary "copy" flag state.
+ *
+ * @param {number} duration - The time in seconds after which `copyFlag` will be automatically reset to `false`.
+ *
+ * @returns {{
+ * - copyFlag: boolean;
+ * - setCopyFlag: Dispatch<SetStateAction<boolean>>;
+ * 	}} An object containing:
+ * - `copyFlag`: current state of the flag (`true` or `false`),
+ * - `setCopyFlag`: function to set the flag manually.
+ *
+ *
+ * @example
+ * const { copyFlag, setCopyFlag } = useCopyFlag(2000);
+ *
+ * // Show "Copied!" message for 2 seconds
+ * const handleCopy = () => {
+ *   setCopyFlag(true);
+ * };
+ *
+ * return (
+ *   <div>
+ *     <button onClick={handleCopy}> Copy </button>
+ *     {copyFlag && <span> Copied! </span>}
+ *   </div>
+ * );
+ */
 export function useCopyFlag(duration: number) {
     const [copyFlag, setCopyFlag] = useState(false);
     const timeoutRef = useRef<NodeJS.Timeout>(null);
@@ -8,7 +36,7 @@ export function useCopyFlag(duration: number) {
         if (copyFlag) {
             timeoutRef.current = setTimeout(() => {
                 setCopyFlag(false);
-            }, duration);
+            }, duration * 100);
         } else {
             if (timeoutRef.current) {
                 clearTimeout(timeoutRef.current);
