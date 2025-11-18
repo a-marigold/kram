8ebd316c625c5a5013d7ab622461fb2915524868
NextKolya <phelixgluy@gmail.com>
2025-11-18 15:59:43 +0600

    Update AuthStore

--- frontend/store/AuthStore/useAuthStore.ts
+++ frontend/store/AuthStore/useAuthStore.ts
@@ -6,14 +6,24 @@ import type { User } from '@none/shared';
 
 interface AuthStore {
     user: Partial<User> | null;
-    setUser: (userData: Partial<User>) => void;
+    setUser: (userData: Partial<User> | null) => void;
 }
 
 export const useAuthStore = create<AuthStore>()((set) => ({
     user: null,
 
     setUser: (userData) =>
-        set((state) => ({
-            user: state.user ? { ...state.user, ...userData } : userData,
-        })),
+        set((state) => {
+            if (userData === null) {
+                return { user: null };
+            }
+
+            if (Object.keys(userData).length === 0) {
+                return { user: {} };
+            }
+
+            return {
+                user: state.user ? { ...state.user, ...userData } : userData,
+            };
+        }),
 }));
