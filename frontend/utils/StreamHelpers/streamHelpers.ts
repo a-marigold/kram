708a660f3ae95a5450ef5cf0ab64e3c974eac96a
NextKolya <phelixgluy@gmail.com>
2025-11-19 19:51:28 +0600

    Upgrade streamHelpers

--- frontend/utils/StreamHelpers/streamHelpers.ts
+++ frontend/utils/StreamHelpers/streamHelpers.ts
@@ -2,21 +2,26 @@ import type { ZodType } from 'zod';
 
 import { StreamMessageSchema } from '@none/shared';
 
-export function validateStreamMessage(message: unknown) {
+import type { StreamMessage } from '@none/shared';
+
+export function validateStreamMessage(
+    message: unknown
+): message is StreamMessage {
     const parseMessage = StreamMessageSchema.safeParse(message);
 
-    if (parseMessage.success) {
-        return true;
-    }
+    if (!parseMessage.success) return false;
+
+    return true;
 }
 
 export function validateStreamData<T extends ZodType>(
     data: unknown,
 
     schema: T
-) {
+): data is T {
     const parseData = schema.safeParse(data);
-    if (parseData.success) {
-        return true;
-    }
+
+    if (!parseData.success) return false;
+
+    return true;
 }
