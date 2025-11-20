042caae221d62eed19379cbb5403366063add407
NextKolya <phelixgluy@gmail.com>
2025-11-20 23:20:48 +0600

    Add memoized version for PrimaryButton

---
+++ frontend/UI/PrimaryButton/memo/index.ts
@@ -0,0 +1,9 @@
+'use client';
+
+import { memo } from 'react';
+
+import PrimaryButton from '../PrimaryButton';
+
+const MemoPrimaryButton = memo(PrimaryButton);
+
+export default MemoPrimaryButton;
