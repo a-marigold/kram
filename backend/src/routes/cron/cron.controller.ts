78a89947b12a8c57af1306e20be402211e868e39
NextKolya <phelixgluy@gmail.com>
2025-11-22 12:52:03 +0600

    Update /cron/wake route

--- backend/src/routes/cron/cron.controller.ts
+++ backend/src/routes/cron/cron.controller.ts
@@ -13,11 +13,35 @@ export async function wake(
         return reply.code(403).send({ code: 403, message: 'Forbidden' });
     }
 
-    await request.server.prisma.user.findMany();
+    try {
+        await request.server.prisma.user.findMany();
+    } catch (error) {
+        if (error instanceof Error) {
+            return reply
+                .code(500)
+                .send({ code: 500, message: `Prisma error: ${error.message}` });
+        }
 
-    await request.server.redis.set('cron:wake', 1);
+        return reply
+            .code(500)
+            .send({ code: 500, message: 'Uknown prisma error' });
+    }
 
-    await request.server.redis.del('cron:wake');
+    try {
+        await request.server.redis.set('cron:wake', 1);
+
+        await request.server.redis.del('cron:wake');
+    } catch (error) {
+        if (error instanceof Error) {
+            return reply
+                .code(500)
+                .send({ code: 500, message: `Redis error: ${error.message}` });
+        }
+
+        return reply
+            .code(500)
+            .send({ code: 500, message: 'Unknown redis error' });
+    }
 
     return reply.code(200).send({ code: 200, message: 'Waked up' });
 }
