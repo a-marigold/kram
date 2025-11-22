fdc4beca2940e8c7b5c37e4ae4aae5ac845b09f1
NextKolya <phelixgluy@gmail.com>
2025-11-22 13:55:30 +0600

    FUCK PRISMA, RENDER, DOCKER. try to fix them

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-mkdir -p /app/certs
-echo "$AIVEN_CA_CERT" > /app/certs/ca.pem
-export NODE_EXTRA_CA_CERTS=/app/certs/ca.pem
+mkdir -p /app/backend/certs
+echo "$AIVEN_CA_CERT" > /app/backend/certs/ca.pem
+export NODE_EXTRA_CA_CERTS=/app/backend/certs/ca.pem
 
 bunx prisma migrate deploy
 bunx prisma generate
